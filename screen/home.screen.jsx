1a6d3ccd15bdc2ba357ea5d465f8389a421391fd
Nasratie <nasratie@MacBook-Pro-de-Nasratie.local>
2024-01-24 14:56:04 +0100

    exo 5

---
+++ screen/home.screen.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text } from 'react-native-paper';
+
+const Home = () => (
+    
+    <View>
+        <Text>
+        Ce ci sera la page Home
+        </Text>
+    </View>
+ 
+);
+
+export default Home;
